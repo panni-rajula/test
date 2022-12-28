41c26b80aca3722b8c96974c3c8cf13eed5598f2
panni-rajula <798134287p@gmail.com>
2022-12-28 19:51:22 +0530

    second commit

---
+++ average_of the_given_student.py
@@ -0,0 +1,7 @@
+n = int(input())
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+print(student_marks)
